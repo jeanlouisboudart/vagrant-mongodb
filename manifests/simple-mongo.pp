20ee659b6ba486936acea2d04fa215e9ae8d8e58
Jean-Louis Boudart <jeanlouis.boudart@gmail.com>
2014-03-11 15:31:59 +0100

    add genghisapp as mongo web ui listining on port 80

--- manifests/simple-mongo.pp
+++ manifests/simple-mongo.pp
@@ -28,3 +28,35 @@ exec { 'dex':
    require => Package['python-pip'],
    
 }
+
+
+package {'gem':
+    ensure => present,
+}
+
+exec {'install-genghisapp':
+      command => "gem install genghisapp",
+    path    => "/usr/bin/:/bin/",
+    require => Package['gem'],
+}
+
+package {'build-essential':
+    ensure => present,
+}
+
+package {'ruby1.9.1-dev':
+    ensure => present,
+    require => Package['build-essential'],
+}
+
+exec {'install-bson_ext':
+      command => "gem install bson_ext",
+    path    => "/usr/bin/:/bin/",
+    require => Package['ruby1.9.1-dev'],
+}
+
+exec {'run-genghisapp':
+      command => "genghisapp --port 80",
+    path    => "/usr/local/bin/:/usr/bin/:/bin/",
+    require => [Exec['install-bson_ext'], Exec['install-genghisapp']],
+}
